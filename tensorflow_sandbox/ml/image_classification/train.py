1f56d153eb9b2760753d358636a5a94aa67cee94
Bryan Chan <bryan.chan@kindred.ai>
2019-06-08 21:45:08 -0400

    Added L2 regularization

--- tensorflow_sandbox/ml/image_classification/train.py
+++ tensorflow_sandbox/ml/image_classification/train.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.datasets.mnist as mnist
@@ -6,54 +7,102 @@ from datetime import datetime
 from models.fully_connected_classifier import FullyConnectedClassifier
 from models.cnn_classifier import CNNClassifier
 
-data_path = "/Users/bryan/Documents/sandbox/data/mnist.npz"
-debug = True
-
-validation_ratio = 0.2
-lr = 3e-4
-weight_decay = 0.1
-epochs = 100
-
 def get_validation_split_indicies(data_length, val_ratio=0.2):
     validation_amount = int(data_length * val_ratio)
     p = np.random.choice(range(data_length), data_length, replace=False)
     return p[:-validation_amount], p[-validation_amount:]
 
-# Get data from MNIST
-(train_x, train_y), (test_x, test_y) = mnist.load_data(path=data_path)
-train_idx, valid_idx = get_validation_split_indicies(len(train_x), validation_ratio)
+def get_mnist_data(data_path, validation_ratio):
+    # Get data from MNIST
+    (train_x, train_y), (test_x, test_y) = mnist.load_data(path=data_path)
+    train_idx, valid_idx = get_validation_split_indicies(len(train_x), validation_ratio)
 
-validation_x = train_x[valid_idx]
-validation_y = train_y[valid_idx]
+    validation_x = train_x[valid_idx]
+    validation_y = train_y[valid_idx]
 
-train_x = train_x[train_idx]
-train_y = train_y[train_idx]
+    train_x = train_x[train_idx]
+    train_y = train_y[train_idx]
 
-train_x = np.expand_dims(train_x, axis=1).transpose(0, 2, 3, 1)
+    # Resize the input
+    train_x = np.expand_dims(train_x, axis=1).transpose(0, 2, 3, 1)
+    validation_x = np.expand_dims(validation_x, axis=1).transpose(0, 2, 3, 1)
 
-validation_x = np.expand_dims(validation_x, axis=1).transpose(0, 2, 3, 1)
+    return train_x, train_y, validation_x, validation_y, test_x, test_y
 
-height, width = train_x.shape[1:3]
-hidden_output_sizes = [100]
-output_size = 10
+def train(data_path, validation_ratio, lr, epochs, dropout, l2_coef, debug):
+    train_x, train_y, validation_x, validation_y, test_x, test_y = get_mnist_data(data_path, validation_ratio)
+    height, width = train_x.shape[1:3]
 
-tf.reset_default_graph()
+    hidden_sizes = [100]
+    output_size = 10
+    latent_size = 32
+    filter_channels=[32]
 
-with tf.name_scope("placeholders"):
-    x = tf.placeholder(tf.float32, (len(train_x), height, width, 1))
-    y = tf.placeholder(tf.int64, (len(train_y),))
+    tf.reset_default_graph()
 
-# model = FullyConnectedClassifier(input_size=(height * width), output_size=10, hidden_sizes=hidden_output_sizes)
-model = CNNClassifier(height=28, width=28, num_channels=1, num_filters=128, output_size=10, hidden_sizes=[128])
+    with tf.name_scope("placeholders"):
+        x = tf.placeholder(tf.float32, (None, height, width, 1))
+        y = tf.placeholder(tf.int64, (None,))
 
-logits = model(x)
-loss = tf.losses.sparse_softmax_cross_entropy(y, logits)
-trainer = tf.train.AdamOptimizer(lr).minimize(loss)
+    # model = FullyConnectedClassifier(input_size=(height * width), output_size=10, hidden_sizes=hidden_output_sizes)
+    model = CNNClassifier(height=28, width=28, input_channels=1, latent_size=latent_size, filter_channels=filter_channels, output_size=output_size, hidden_sizes=hidden_sizes)
 
-with tf.Session() as sess:
-    sess.run(tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()))
-    for epoch in range(epochs):
-        _, train_loss = sess.run([trainer, loss], feed_dict={x: train_x, y: train_y})
-        if (epoch + 1) % 10 == 0:
+    logits = model(x)
+    predictions = tf.argmax(logits, 1)
+
+    model_weights, _ = model.parameters
+
+    loss = tf.losses.sparse_softmax_cross_entropy(y, logits)
+    for weight in model_weights:
+        loss += l2_coef * tf.nn.l2_loss(weight)
+        
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(y, predictions), tf.float32))
+    opt = tf.train.AdamOptimizer(lr).minimize(loss)
+
+    with tf.name_scope("summaries"):
+        tf.summary.scalar("loss", loss)
+        tf.summary.scalar("accuracy", accuracy)
+        merged = tf.summary.merge_all()
+
+    with tf.Session() as sess:
+        if not debug:
+            start_time = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
+            train_writer = tf.summary.FileWriter(logdir="./runs/{}_train".format(start_time))
+            validation_writer = tf.summary.FileWriter(logdir="./runs/{}_validation".format(start_time))
+
+        sess.run(tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()))
+        for epoch in range(epochs):
+            if not debug:
+                _, train_summary, train_loss, train_accuracy = sess.run([opt, merged, loss, accuracy], feed_dict={x: train_x, y: train_y})
+                train_writer.add_summary(train_summary, epoch)
+                
+                validation_summary, validation_loss, validation_accuracy = sess.run([merged, loss, accuracy], feed_dict={x: validation_x, y: validation_y})
+                validation_writer.add_summary(validation_summary, epoch)
+            else:
+                _, train_loss, train_accuracy = sess.run([opt, loss, accuracy], feed_dict={x: train_x, y: train_y})
+                validation_loss, validation_accuracy = sess.run([loss, accuracy], feed_dict={x: validation_x, y: validation_y})
+
+            if (epoch + 1) % 10 == 0:
                 print("Epoch: {} ==================".format(epoch + 1))
-                print("Training - Loss: {}".format(train_loss))
+                print("Training - Loss: {}, Accuracy: {}".format(train_loss, train_accuracy))
+                print("Validation - Loss: {}, Accuracy: {}".format(validation_loss, validation_accuracy))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default="/Users/bryan/Documents/sandbox/data/mnist.npz", 
+                        help='The location of the data file')
+    parser.add_argument('--validation_ratio', type=float, default=0.2,
+                        help='Train validation split ratio')
+    parser.add_argument('--lr', type=float, default=3e-4, 
+                        help='The learning rate')
+    parser.add_argument('--epochs', type=int, default=100, 
+                        help='Number of epochs to train')
+    parser.add_argument('--dropout', type=float, default=0, 
+                        help='The dropout rate')
+    parser.add_argument('--l2_coef', type=float, default=0.1,
+                        help='The coefficient for L2 regularization')
+    parser.add_argument('--debug', action='store_true', 
+                        help='Debug mode')
+    args = parser.parse_args()
+
+    train(args.data_path, args.validation_ratio, args.lr, args.epochs, args.dropout, args.l2_coef, args.debug)
--- tensorflow_sandbox/models/fully_connected_classifier.py
+++ tensorflow_sandbox/models/fully_connected_classifier.py
@@ -5,31 +5,40 @@ from layers import FullyConnectedLayer, Flatten
 class FullyConnectedClassifier():
     def __init__(self, input_size, output_size, hidden_sizes=None, activation=tf.nn.relu, dropout=0.0):
         self._dropout = dropout
-        self.flatten = Flatten()
+        self._flatten = flatten()
 
         layer_dimensions = [input_size]
-
         if hidden_sizes:
             layer_dimensions.extend(hidden_sizes)
-
         layer_dimensions.append(output_size)
 
-        self.layers = []
+        self._layers = []
 
         for i in range(len(layer_dimensions) - 1):
             if i == len(layer_dimensions) - 2:
-                self.layers.append(FullyConnectedLayer(input_size=layer_dimensions[i], 
+                self._layers.append(FullyConnectedLayer(input_size=layer_dimensions[i], 
                                     output_size=layer_dimensions[i + 1], activation=None, name="fully_connected_{}".format(i)))
                 continue
 
-            self.layers.append(FullyConnectedLayer(input_size=layer_dimensions[i], 
+            self._layers.append(FullyConnectedLayer(input_size=layer_dimensions[i], 
                                 output_size=layer_dimensions[i + 1], activation=activation, name="fully_connected_{}".format(i)))
 
-
     def __call__(self, x):
-        x = self.flatten(x)
-        for layer in self.layers:
+        x = self._flatten(x)
+        for layer in self._layers:
             x = layer(x)
             x = tf.nn.dropout(x, rate=self._dropout)
 
         return x
+
+    def _get_parameters(self):
+        self._weights = []
+        self._biases = []
+
+        for layer in self._layers:
+            self._weights.append(layer.weights)
+            self._biases.append(layer.bias)
+
+    @property
+    def parameters(self):
+        return self._weights, self._biases
