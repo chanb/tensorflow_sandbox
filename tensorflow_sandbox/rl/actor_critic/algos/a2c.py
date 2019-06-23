b91f880ae82a11409b24dffc9b1d648a5f889d2c
Bryan Chan <bryan.chan@kindred.ai>
2019-06-22 22:45:55 -0400

    Added a2c

--- tensorflow_sandbox/rl/actor_critic/algos/a2c.py
+++ tensorflow_sandbox/rl/actor_critic/algos/a2c.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 
 
-class REINFORCE():
+class A2C():
     def __init__(self, sess, env, agent, storage,
                  max_timesteps=100000, discount=0.99, lr=3e-4,
                  actor_coef=1., critic_coef=0.5, ent_coef=0.):
@@ -24,10 +24,10 @@ class REINFORCE():
     def _build_algo(self):
         # Construct loss and optimizer
         with tf.variable_scope("loss_opt"):
-            self._returns = tf.placeholder(
+            self._rewards = tf.placeholder(
                 tf.float32,
                 [None],
-                name="returns"
+                name="rewards"
             )
 
             self._actions_taken = tf.placeholder(
@@ -42,16 +42,18 @@ class REINFORCE():
 
             # Get log prob and policy entropy
             self._log_probs = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                logits=self._logits, labels=self._actions_taken)
+                logits=self._logits[:-1], labels=self._actions_taken)
 
             self._entropies = tf.nn.softmax_cross_entropy_with_logits_v2(
-                logits=self._logits, labels=self._logits)
+                logits=self._logits[:-1], labels=self._logits[:-1])
 
             # Compute actor, critic, and entropy losses
+            self._advantage = self._rewards + self._values[1:] - \
+                self._values[:-1]
             self._critic_loss = tf.reduce_mean(
-                tf.square(self._returns - self._values))
+                tf.square(self._advantage))
             self._actor_loss = tf.reduce_mean(
-                self._log_probs * (self._returns - self._values))
+                self._log_probs * self._advantage)
             self._entropy_loss = tf.reduce_mean(
                 self._entropies
             )
@@ -65,12 +67,8 @@ class REINFORCE():
     def _update(self):
         states, actions, rewards, policy_probs, dones = self._storage.get_all()
 
-        states = np.array(states[:-1])
+        states = np.array(states)
         actions = np.array(actions, dtype=np.int32)
-        returns = np.zeros(shape=(len(rewards)))
-        for idx, reward in enumerate(rewards[::-1]):
-            returns[:-idx] += reward
-            returns[:-idx] *= self._discount
 
         _, loss, actor_loss, critic_loss, entropy_loss = sess.run(
             [self._opt, self._loss, self._actor_loss, self._critic_loss,
@@ -78,31 +76,41 @@ class REINFORCE():
             feed_dict={
                 agent.state_input: states,
                 self._actions_taken: actions,
-                self._returns: returns
+                self._rewards: rewards
                 }
         )
 
-        print(("Loss: {}, Actor Loss: {}, Critic Loss: {}"
-               ", Entropy: {} Return: {}").format(
-                loss, actor_loss, critic_loss, entropy_loss, sum(rewards)))
+        return loss, actor_loss, critic_loss, entropy_loss
 
     def learn(self):
         done = False
         state = self._env.reset()
         self._storage.states.append(state)
 
-        for _ in range(self._max_timesteps):
+        ep = 1
+        curr_return = 0
+        for timestep in range(self._max_timesteps):
             self._env.render()
             action, probs, value = agent.act(np.expand_dims(state, axis=0))
             state, reward, done, info = self._env.step(action)
+            curr_return += reward
             self._storage.add(state, action, reward, probs, value, done)
+            loss, actor_loss, critic_loss, entropy_loss = algo._update()
+
+            if timestep % 100 == 0:
+                print(("Loss: {}, Actor Loss: {}, Critic Loss: {}"
+                       ", Entropy: {}").format(
+                        loss, actor_loss, critic_loss, entropy_loss))
 
             if done:
-                algo._update()
+                print("Episode {} ended - Return: {}".format(ep, curr_return))
+                ep += 1
+                curr_return = 0
                 done = False
                 state = self._env.reset()
-                self._storage.reset()
-                self._storage.states.append(state)
+
+            self._storage.reset()
+            self._storage.states.append(state)
 
 
 if __name__ == "__main__":
@@ -120,14 +128,14 @@ if __name__ == "__main__":
         storage = Storage()
         agent = Agent(action_dim=env.action_space.n,
                       state_dim=env.observation_space.shape)
-        algo = REINFORCE(sess=sess,
-                         env=env,
-                         agent=agent,
-                         storage=storage,
-                         max_timesteps=max_timesteps,
-                         lr=0.01,
-                         critic_coef=0.7,
-                         ent_coef=0.0002)
+        algo = A2C(sess=sess,
+                   env=env,
+                   agent=agent,
+                   storage=storage,
+                   max_timesteps=max_timesteps,
+                   lr=1e-3,
+                   critic_coef=0.7,
+                   ent_coef=0.0002)
 
         tf.global_variables_initializer().run()
         algo.learn()
